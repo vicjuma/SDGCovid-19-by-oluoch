1f00879b03b9383f6cc5df30dd6c12b9be2dce48
vicjuma <vicjuma945@gmail.com>
2020-04-09 16:28:49 -0700

    estimating the currentlyInfected and infectionsByRequestedTime figures

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,12 @@
-const covid19ImpactEstimator = (data) => data;
-
+const covid19ImpactEstimator = (data) => ({
+  data,
+  impact: {
+    currentlyInfected: data.reportedCases * 10,
+    infectionsByRequestedTime: (data.reportedCases * 10) * (2 ** 9)
+  },
+  severeImpact: {
+    currentlyInfected: data.reportedCases * 50,
+    infectionsByRequestedTime: (data.reportedCases * 50) * (2 ** 9)
+  }
+});
 export default covid19ImpactEstimator;
