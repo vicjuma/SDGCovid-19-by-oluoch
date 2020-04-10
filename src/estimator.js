0fb241c2c5963bd1a29f8482c992774f72f461ae
vicjuma <vicjuma945@gmail.com>
2020-04-09 20:53:24 -0700

    --no-message

--- src/estimator.js
+++ src/estimator.js
@@ -1,24 +1,27 @@
-const impact = (data) => {
-  if (data.periodType === 'days') {
-    return (Math.trunc((data.timeToElapse * 1) / 3));
-  }
-  if (data.periodType === 'weeks') {
-    return (Math.trunc((data.timeToElapse * 7) / 3));
-  }
-  if (data.periodType === 'months') {
-    return (Math.trunc((data.timeToElapse * 30) / 3));
-  }
-  return 0;
+const covid19ImpactEstimator = (data) => {
+  const impact = () => {
+    if (data.periodType === 'days') {
+      return (Math.trunc((data.timeToElapse * 1) / 3));
+    }
+    if (data.periodType === 'weeks') {
+      return (Math.trunc((data.timeToElapse * 7) / 3));
+    }
+    if (data.periodType === 'months') {
+      return (Math.trunc((data.timeToElapse * 30) / 3));
+    }
+    return 0;
+  };
+  return {
+    data,
+    impact: {
+      currentlyInfected: data.reportedCases * 10,
+      infectionsByRequestedTime: (data.reportedCases * 10) * (2 ** (impact()))
+    },
+    severeImpact: {
+      currentlyInfected: data.reportedCases * 50,
+      infectionsByRequestedTime: (data.reportedCases * 50) * (2 ** (impact()))
+    }
+  };
 };
-const covid19ImpactEstimator = (data) => ({
-  data,
-  impact: {
-    currentlyInfected: data.reportedCases * 10,
-    infectionsByRequestedTime: (data.reportedCases * 10) * (2 ** (impact(data)))
-  },
-  severeImpact: {
-    currentlyInfected: data.reportedCases * 50,
-    infectionsByRequestedTime: (data.reportedCases * 50) * (2 ** (impact(data)))
-  }
-});
+
 export default covid19ImpactEstimator;
