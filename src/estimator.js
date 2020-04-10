373c357ab5cb4d3e998880a39e91b22b21055608
vicjuma <vicjuma945@gmail.com>
2020-04-09 22:02:17 -0700

    --no-message

--- src/estimator.js
+++ src/estimator.js
@@ -16,12 +16,14 @@ const covid19ImpactEstimator = (data) => {
     impact: {
       currentlyInfected: data.reportedCases * 10,
       infectionsByRequestedTime: (data.reportedCases * 10) * (2 ** (impact())),
-      severeCasesByRequestedTime: (data.reportedCases * 10) * (2 ** (impact())) * Math.trunc(15 / 100)
+      severeCasesByRequestedTime: (data.reportedCases * 10)
+      * (2 ** (impact())) * Math.trunc(15 / 100)
     },
     severeImpact: {
       currentlyInfected: data.reportedCases * 50,
       infectionsByRequestedTime: (data.reportedCases * 50) * (2 ** (impact())),
-      severeCasesByRequestedTime: (data.reportedCases * 50) * (2 ** (impact())) * Math.trunc(15 / 100)
+      severeCasesByRequestedTime: (data.reportedCases * 50)
+      * (2 ** (impact())) * Math.trunc(15 / 100)
     }
   };
 };
