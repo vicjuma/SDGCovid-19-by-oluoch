894f98e13f25d5e093010a1502d5ac7dea904f9a
vicjuma <vicjuma945@gmail.com>
2020-04-09 19:20:12 -0700

    --no-message

--- src/estimator.js
+++ src/estimator.js
@@ -1,33 +1,36 @@
-const covid19ImpactEstimator = (data) => {
-  const conversionFunc = (periodType) => {
-    let days;
-    switch (periodType) {
-      case 'days':
-        days = data.timeToElapse * 1;
-        break;
-      case 'weeks':
-        days = data.timeToElapse * 7;
-        break;
-      case 'months':
-        days = data.timeToElapse * 30;
-        break;
-      default:
-        days = data.timeToElapse * 1;
-    }
-    return days;
-  };
-  return {
-    data,
-    impact: {
-      currentlyInfected: data.reportedCases * 10,
-      infectionsByRequestedTime: (data.reportedCases * 10)
-      * (2 ** (conversionFunc(data.periodType) / 3))
-    },
-    severeImpact: {
-      currentlyInfected: data.reportedCases * 50,
-      infectionsByRequestedTime: (data.reportedCases * 50)
-      * (2 ** (conversionFunc(data.periodType) / 3))
-    }
-  };
+const impact = (data) => {
+  if (data.periodType === 'days') {
+    return (Math.trunc(data.timeToElapse / 3));
+  }
+  if (data.periodType === 'weeks') {
+    return (Math.trunc((data.timeToElapse * 7) / 3));
+  }
+  if (data.periodType === 'months') {
+    return (Math.trunc((data.timeToElapse * 30) / 3));
+  }
+  return 0;
 };
+const covid19ImpactEstimator = (data) => ({
+  data: {
+    region: {
+      name: 'Africa',
+      avgAge: 19.7,
+      avgDailyIncomeInUSD: 5,
+      avgDailyIncomePopulation: 0.71
+    },
+    periodType: 'days',
+    timeToElapse: 58,
+    reportedCases: 674,
+    population: 66622705,
+    totalHospitalBeds: 1380614
+  },
+  impact: {
+    currentlyInfected: data.reportedCases * 10,
+    infectionsByRequestedTime: (data.reportedCases * 10) * (2 ** (impact(data)))
+  },
+  severeImpact: {
+    currentlyInfected: data.reportedCases * 50,
+    infectionsByRequestedTime: (data.reportedCases * 50) * (2 ** (impact(data)))
+  }
+});
 export default covid19ImpactEstimator;
